9fb56a8f3a1a1fc8c134bdb5a79c5f98b2956d3e
Swaraj961 <swarajroutray961@gmail.com>
2020-06-27 00:08:44 +0530

    resume Updated

--- lib/tabs/about_tab.dart
+++ lib/tabs/about_tab.dart
@@ -249,7 +249,7 @@ class AboutTab extends StatelessWidget {
             color: Colors.deepOrangeAccent,
             onPressed: () {
               html.window.open(
-                  "https://drive.google.com/open?id=1m_aeXVWc3pFHzcNcNtchL4ZsFHdEFSPf",
+                  "https://drive.google.com/file/d/1RAOc1wyCjMFUq4C9KuEquFxVMYPz-DZw/view",
                   "");
             },
             padding: EdgeInsets.all(10),
